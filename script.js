37728ad7eb7b1929e7bba4be0a995d8cf8b37868
Bryn Murrell <brynmurrell97@gmail.com>
2016-12-14 15:10:15 +1100

    adding js

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#g").click(function(){
+        $("#a").fadeOut();
+        $("#p").fadeOut();
+        $("#c").fadeOut();
+    });
+//    $("#show").click(function(){
+//        $("p").show();
+//    });
+});
